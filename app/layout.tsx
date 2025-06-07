0a3b9b0adb14fdc30c6cdf3c746cfc1854e9ca1a
Myhailo Nyskohuz <mn508678@gmail.com>
2025-06-07 11:42:53 +0200

    manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,15 +54,18 @@ export default function RootLayout({
     <html lang="en">
       <Head>
   <title>My App</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui, viewport-fit=cover" />
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
   <meta name="apple-mobile-web-app-title" content="My App" />
+  <meta name="apple-touch-fullscreen" content="yes" />
+  <link rel="manifest" href="/manifest.json" />
   <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 </Head>
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[110vh]`}
       >
         {children}
       </body>
